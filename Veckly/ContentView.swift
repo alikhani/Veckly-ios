242f4065ee2a6a6d54e5280f755eae41661d8adf
alikhani <nimaalikhani5@gmail.com>
2026-06-09 21:58:16 +0200

    init

---
+++ Veckly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Veckly
+//
+//  Created by Nima on 2026-06-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Veckly/VecklyApp.swift
@@ -0,0 +1,17 @@
+//
+//  VecklyApp.swift
+//  Veckly
+//
+//  Created by Nima on 2026-06-09.
+//
+
+import SwiftUI
+
+@main
+struct VecklyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
